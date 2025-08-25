f78e037d833f30d83dcc3292835b89cf4251f262
Mahsa Fateh <mahsafaateh@gmail.com>
2025-08-25 14:35:33 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mahsa Fateh on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
